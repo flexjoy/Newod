6d0b88c099827fbb6d88701b9db12df4660afddd
Sergey Cherepanov <flexjoy@ya.ru>
2016-04-01 09:14:20 +0300

    add Spring Boot Application class

---
+++ src/main/java/flexjoy/newod/Application.java
@@ -0,0 +1,16 @@
+package flexjoy.newod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Sergey Cherepanov on 01.04.2016
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
