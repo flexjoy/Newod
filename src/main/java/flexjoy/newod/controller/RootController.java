d98c292d4b501bd0daa8a866780a2b0e14686696
Sergey Cherepanov <flexjoy@ya.ru>
2016-04-08 22:54:30 +0300

    add AngularJS routing
    Partial page `store-select` loading via RootController. Partials as well as the `index` page are fully thymeleaf compatible.

--- src/main/java/flexjoy/newod/controller/RootController.java
+++ src/main/java/flexjoy/newod/controller/RootController.java
@@ -1,6 +1,9 @@
 package flexjoy.newod.controller;
 
+import flexjoy.newod.repository.DivisionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -10,8 +13,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class RootController {
 
+	@Autowired
+	private DivisionRepository repo;
+
 	@RequestMapping("/")
 	public String index() {
 		return "index";
+	}
+
+	@RequestMapping("home")
+	public String home() {
+		return "partials/home";
+	}
+
+	@RequestMapping("store-select")
+	public String stores(Model model) {
+		model.addAttribute("divisions", repo.findAll());
+		return "partials/store-select";
 	}
 }
--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -2,4 +2,22 @@
  * Created by SCherepanov on 08.04.2016.
  */
 
-var newodApp = angular.module('newodApp', []);
+var newodApp = angular.module('newodApp', ['ngRoute']);
+
+
+newodApp.config(function ($routeProvider) {
+	$routeProvider.
+	when('/store-select', {
+		templateUrl: 'store-select',
+		controller: 'StoreSelectController'
+	}).
+	when('/home', {
+		templateUrl: 'home',
+		controller: 'HomeController'
+	}).
+	otherwise({redirectTo: 'home'});
+});
+
+newodApp.controller('StoreSelectController', function($scope) {});
+
+newodApp.controller('HomeController', function($scope) {});
