8c29d32286637e29c5c39cc62539f22dc9de2017
Sergey Cherepanov <flexjoy@ya.ru>
2016-06-02 15:59:10 +0300

    Add toast on success/error store save.

--- src/main/resources/static/entities/store-detail/state.js
+++ src/main/resources/static/entities/store-detail/state.js
@@ -14,20 +14,28 @@ app.config(function ($stateProvider) {
 					return City.getAll().$promise;
 				}
 			},
-			controller: function ($scope, store, cities, Store) {
+			controller: function ($scope, store, cities, Store, ToastService, $filter) {
 				var vm = this;
+				var $translate = $filter('translate');
 				$scope.store = angular.copy(store);
 				$scope.cities = cities;
 				vm.save = save;
 				vm.reset = reset;
 
-				function save(store) {
-					Store.update(store);
+				function save(store, form) {
+					Store.update(store, function () {
+						ToastService.Success($translate('TEXT.updated'));
+						form.$setPristine();
+					}, onError);
 				}
 
 				function reset(form) {
 					angular.extend($scope.store, store);
 					form.$setPristine();
+				}
+
+				function onError(error) {
+					ToastService.Error(error.data.error);
 				}
 			},
 			controllerAs: 'vm'
