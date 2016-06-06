0d7ebb66fc970d3a93e36de16dab680e5c69bfa5
Sergey Cherepanov <flexjoy@ya.ru>
2016-06-06 21:54:47 +0300

    Change tab links as separate nested states of store-detail parent state.

--- src/main/resources/static/entities/store-detail/state.js
+++ src/main/resources/static/entities/store-detail/state.js
@@ -4,14 +4,27 @@ app.config(function ($stateProvider) {
 
 	$stateProvider
 		.state('store-detail', {
-			url: "/stores/:id",
-			templateUrl: "entities/store-detail/store.html",
+			url: '/stores/:id',
+			templateUrl: 'entities/store-detail/store.html',
 			onEnter: function ($stateParams, Data) {
 				Data.getStore($stateParams.id);
 				Data.refreshCities();
 			},
 			controller: 'StoreDetailController',
 			controllerAs: 'vm'
+		})
+		.state('store-detail.contacts', {
+			url: '/contacts',
+			templateUrl: 'entities/store-detail/partials/contacts.html'
+		})
+		.state('store-detail.files', {
+			url: '/files'
+		})
+		.state('store-detail.connections', {
+			url: '/connections'
+		})
+		.state('store-detail.equipment', {
+			url: '/equipment'
 		})
 });
 
